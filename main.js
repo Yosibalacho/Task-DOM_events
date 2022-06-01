b97766c270aa912fa30b387a2f2e00b49e5f1327
Yosibalacho <yosibala@gmail.com>
2022-06-01 18:44:23 +0300

    events

---
+++ main.js
@@ -0,0 +1,69 @@
+//1//
+// function alertFunc() {
+//   alert("Hello nigga");
+// }
+//2//
+// function logFunc() {
+//   console.log("King Yosi Balacho");
+// }
+//3//
+// function colorChange() {
+//   span1.style.color = "yellow";
+// }
+//4//
+// function bigH1() {
+//   h1_1.style.width = "200px";
+// }
+//5//
+// function textChange(params) {
+//     pChange.innerText="Yes we do"
+// }
+//6//
+// var userName = prompt("Enter your name");
+
+// function clickName() {
+//   document.write(userName);
+// }
+//7//
+// function myFunc() {
+//   var myDate = new Date();
+//   if ( myDate.getHours()< 12) {
+//     document.body.style.background = "yellow";
+//     h1_day.innerText="Good day"
+
+//   } else {
+//     document.body.style.background = "blue";
+//     h2_eve.innerText="Good evening"
+//   }
+// }
+// myFunc();
+//8//
+// window.onload= function () {
+//   var userAge = prompt("Enter your age");
+//   if (userAge > 18) {
+//     h1_red.innerText = "welcome";
+//     h1_red.style.color="red"
+//   } else {
+//     h2_blue.innerText = "welcome";
+//     h2_blue.style.color="blue"
+//   }
+// }
+// function hoverFunc() {
+//     h1_red.style.color="green"
+//     h2_blue.style.color="green"
+// }
+//9//10//
+
+// function lottoFunc() {
+//   for (var i = 0; i < 5; i++) {
+//     var numBetween0to56 = +prompt("Enter number 0-56");
+//     document.write(numBetween0to56 + " ");
+//   }
+//   if (numBetween0to56 == rnd) {
+//     alert("Sucsses");
+//   } else {
+//     alert("Fail");
+//   }
+//   var rnd = Math.floor(Math.random() * 57);
+//   document.write("<br>" + "<br>" + rnd);
+// }
